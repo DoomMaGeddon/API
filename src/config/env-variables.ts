b70b402db5ca963a9aa1b8d42c7d47dabc688656
DoomMaGeddon <luismi_ale@hotmail.es>
2024-05-30 21:14:24 +0200

    fixes

--- src/config/env-variables.ts
+++ src/config/env-variables.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const HOST = process.env.HOST || "0.0.0.0";
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST ?? "localhost";
+export const DB_HOST = process.env.DB_HOST ?? "1.1.1.1";
 export const DB_PORT = Number(process.env.DB_PORT) ?? 5432;
 export const DB_USERNAME = process.env.DB_USERNAME ?? "";
 export const DB_PASSWORD = process.env.DB_PASSWORD ?? "";
